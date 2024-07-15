603181bacbd9c6896f3ec1b78f019e224826a72a
siddharthtated <143174310+siddharthtated@users.noreply.github.com>
2024-07-15 15:49:05 +0530

    Update login.py

--- login.py
+++ login.py
@@ -2,4 +2,5 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-class 
+class login:
+  login
